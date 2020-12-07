9f16fb8dba54e1934e4c27f4489deafcf9777c73
sunanie <sunanie@qq.com>
2020-12-07 15:02:20 +0800

    修改标题和logo

--- vue/src/layouts/BasicLayout.vue
+++ vue/src/layouts/BasicLayout.vue
@@ -47,7 +47,7 @@ import defaultSettings from '@/config/defaultSettings'
 import RightContent from '@/components/GlobalHeader/RightContent'
 import GlobalFooter from '@/components/GlobalFooter'
 import Ads from '@/components/Other/CarbonAds'
-import LogoSvg from '../assets/logo.svg?inline'
+import LogoSvg from '../assets/little_bird_logo.svg?inline'
 
 export default {
   name: 'BasicLayout',
