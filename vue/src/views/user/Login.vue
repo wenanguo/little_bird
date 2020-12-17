207e439e36f046b24d3531da5d85989707eab545
Andrew Wen <wenanguo110@163.com>
2020-12-17 15:21:32 +0800

    修改首页载入文字

--- vue/src/views/user/Login.vue
+++ vue/src/views/user/Login.vue
@@ -100,7 +100,7 @@
         <a>
           <a-icon class="item-icon" type="weibo-circle"></a-icon>
         </a> -->
-        <router-link class="register" :to="{ name: 'register' }">注册账户</router-link>
+        <!-- <router-link class="register" :to="{ name: 'register' }">注册账户</router-link> -->
       </div>
     </a-form>
 
